25f30df555489c10fb7c451f2ae5a44da5cd90b8
YA <vector_cp@mail.ru>
2021-05-08 19:47:56 +0300

    refactor: basic refactor to stream

--- index.js
+++ index.js
@@ -1,84 +1,147 @@
 import * as fs from "fs"
-import path from "path"
-import { URL } from "url"
+import  { pipeline }  from "stream/promises"
 import chalk from "chalk"
 
 import handleData from "./handleData.js"
-
+import encodeCaesar from "./encodeCaesar.js"
 
 const rawArgs = process.argv
 const args = handleData(rawArgs)
 console.log(args)
 
-
-//check for args
 if (!args.action || !args.shift) {
   process.exitCode = 1
-  process.stderr.write(chalk.magentaBright.inverse(` Error `)+
-    ` Has no ${args.action ? "" : "action"}${
-      !args.action && !args.shift ? "," : ""
-    } ${args.shift ? "" : "shift"} argument`
+  process.stderr.write(
+    chalk.magentaBright.inverse(` Error `) +
+      ` Has no ${args.action ? "" : "action"}${
+        !args.action && !args.shift ? "," : ""
+      } ${args.shift ? "" : "shift or shift = 0"} argument`
   )
   process.exit()
 }
 
-//funcs
-const encodeCaesar = (data = "", shift) => {
-  const inputArray = data.split("")
-  const encodedArray = inputArray.map((el) => {
-    if (el.charCodeAt(0) >= 97 && el.charCodeAt(0) <= 122) {
-      return String.fromCharCode(
-        (((el.charCodeAt(0) + shift) % 123) % 97) + 97
-      )
-    } else if (
-      el.charCodeAt(0) >= 65 &&
-      el.charCodeAt(0) <= 90
-    ) {
-      return String.fromCharCode(
-        (((el.charCodeAt(0) + shift) % 91) % 65) + 65
-      )
-    } else return el
-  })
 
-  // console.log("encodeCaesar", encodedArray.join(""), shift)
-  return encodedArray.join("")
-}
-
-//-----
-
-fs.readFile(args.inputFile, "utf8", (err, data) => {
-  if (err) {
-    process.exitCode = 1
-    process.stderr.write(chalk.magentaBright.inverse(` Error `)+` Can't read source file ${args.inputFile}`)
-    process.exit()
-  }
-  const encString = encodeCaesar(data, args.shift)
-
-  if (args.outputFile) {
-
-    fs.access(args.outputFile, function(error){
-      if (error) {
-        process.exitCode = 1
-        process.stderr.write(chalk.magentaBright.inverse(` Error `)+` Can't find file ${args.outputFile}`)
-        process.exit()
-      } else {
-        fs.appendFile(args.outputFile, encString, (err) => {
-          if (err){
-            process.exitCode = 1
-            process.stderr.write(chalk.magentaBright.inverse(` Error `)+` Can't write to file ${args.outputFile}`)
-            process.exit()
-          }
-          process.stdout.write(chalk.greenBright.inverse(` Success `)+` ${args.outputFile} saved`)
-        });
+async function run() {
+  await pipeline(
+ 
+    true ? process.stdin : fs.createReadStream(args.inputFile),
+   
+     async function* (source) {
+      source.setEncoding('utf8');  
+      for await (const chunk of source) {
+        yield encodeCaesar(chunk, args.shift);
       }
-  });
+    },
+    // async function (source) {
+    //   console.log('-->',source);
+    // }
+   
+  fs.createWriteStream(args.outputFile)
+  );
+  console.log('Pipeline succeeded.');
+}
+ run().catch(console.error);
 
-    
-  } else   process.stdout.write(chalk.greenBright.inverse(` Encoded Output `)+` ${encString}`)
-  
-  
 
-})
+// async function run() {
+//   await pipeline(
+//     fs.createReadStream('input.txt'),
+//     fs.createWriteStream('out.txt')
+//   );
+//   console.log('Pipeline succeeded.');
+// }
+
+// run().catch(console.error);
+
+
+// //check for args
+// if (!args.action || !args.shift) {
+//   process.exitCode = 1
+//   process.stderr.write(
+//     chalk.magentaBright.inverse(` Error `) +
+//       ` Has no ${args.action ? "" : "action"}${
+//         !args.action && !args.shift ? "," : ""
+//       } ${args.shift ? "" : "shift"} argument`
+//   )
+//   process.exit()
+// }
+
+// //funcs
+
+// //-----
+
+// fs.access(args.inputFile, function (error) {
+//   if (error) {
+//     // todo stdin functional
+//     process.stdout.write(
+//       chalk.greenBright.inverse(`Enter the data: `)
+//     )
+
+//     process.stdin.setEncoding('utf8');
+
+//     process.stdin.on('readable', () => {
+//       var chunk = process.stdin.read();
+//       if (chunk !== null) {
+//         process.stdout.write(`data: ${chunk}`);
+//       }
+//     });
+  
+    // process.stdin.on('end', () => {
+    //   process.stdout.write('end');
+    // });
+
+
+   
+//   } else {
+//     fs.readFile(args.inputFile, "utf8", (err, data) => {
+//       if (err) {
+//         process.exitCode = 1
+//         process.stderr.write(
+//           chalk.magentaBright.inverse(` Error `) +
+//             ` Can't read source from ${args.inputFile}`
+//         )
+//         process.exit()
+//       }
+//       const encString = encodeCaesar(data, args.shift)
+
+//       if (args.outputFile) {
+//         fs.access(args.outputFile, function (error) {
+//           if (error) {
+//             process.exitCode = 1
+//             process.stderr.write(
+//               chalk.magentaBright.inverse(` Error `) +
+//                 ` Can't find file ${args.outputFile}`
+//             )
+//             process.exit()
+//           } else {
+//             fs.appendFile(
+//               args.outputFile,
+//               encString,
+//               (err) => {
+//                 if (err) {
+//                   process.exitCode = 1
+//                   process.stderr.write(
+//                     chalk.magentaBright.inverse(` Error `) +
+//                       ` Can't write to file ${args.outputFile}`
+//                   )
+//                   process.exit()
+//                 }
+//                 process.stdout.write(
+//                   chalk.greenBright.inverse(` Success `) +
+//                     ` ${args.outputFile} saved`
+//                 )
+//               }
+//             )
+//           }
+//         })
+//       } else
+//         process.stdout.write(
+//           chalk.greenBright.inverse(` Encoded Output `) +
+//             ` ${encString}`
+//         )
+//     })
+//   }
+// })
 
 // console.log(rawArgs);
 // process.on('exit', (code) => {
