813958ed6d48b5afd6059dfc8e568b84dbb790db
YA <vector_cp@mail.ru>
2021-05-08 13:26:36 +0300

    feat:set basic encode

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
 import fs from "fs"
 import path from "path"
-import { URL } from 'url';
+import { URL } from "url"
 
 import handleData from "./handleData.js"
-const rawArgs = process.argv 
+const rawArgs = process.argv
 const args = handleData(rawArgs)
-console.log(args);
+console.log(args)
 
 if (!args.action || !args.shift) {
-  process.exitCode = 1;
-  process.stderr.write(`Has no ${args.action ? "" : "action"}${(!args.action && !args.shift) ? "," : ""} ${args.shift ? "" : "shift"} argument` )
+  process.exitCode = 1
+  process.stderr.write(
+    `Has no ${args.action ? "" : "action"}${
+      !args.action && !args.shift ? "," : ""
+    } ${args.shift ? "" : "shift"} argument`
+  )
   process.exit()
 }
 
 //funcs
-const encodeCaesar = (data = "", shift = 0) => {
+const encodeCaesar = (data = "", shift) => {
   const LETTERS = 26
   const inputArray = data.split("")
-  const encodedArray = inputArray.map(el => {
-    return el.charCodeAt(0)
+  const encodedArray = inputArray.map((el) => {
+    if (el.charCodeAt(0) >= 97 && el.charCodeAt(0) <= 122) {
+      return  String.fromCharCode(((el.charCodeAt(0)+shift) % 123) % 97 + 97)
+    } else if (
+      el.charCodeAt(0) >= 65 &&
+      el.charCodeAt(0) <= 90
+    ) {
+      return  String.fromCharCode(((el.charCodeAt(0)+shift) % 91) % 65 + 65)
+    } else return el
   })
-  
-  
-  console.log('encodeCaesar', encodedArray, shift )
+
+  console.log("encodeCaesar", encodedArray, shift)
 }
 
 //-----
 
-
-fs.readFile("./input.txt", 'utf8', (err, data) => {
-  if (err) throw err;
+fs.readFile("./input.txt", "utf8", (err, data) => {
+  if (err) throw err
   encodeCaesar(data, args.shift)
-});
-
+})
 
 // console.log(rawArgs);
 // process.on('exit', (code) => {
-  //   console.log(`About to exit with code: ${code}`);
-  // });
-  
-  // const __filename = new URL('', import.meta.url).pathname;
-  // // Will contain trailing slash
-  // const __dirname = new URL('.', import.meta.url).pathname;
-  // console.log('sss', __dirname);
+//   console.log(`About to exit with code: ${code}`);
+// });
 
+// const __filename = new URL('', import.meta.url).pathname;
+// // Will contain trailing slash
+// const __dirname = new URL('.', import.meta.url).pathname;
+// console.log('sss', __dirname);
 
-  // https://abakbot.ru/online-5/301-rus-eng-alphabet
-  
-  
+// https://abakbot.ru/online-5/301-rus-eng-alphabet
