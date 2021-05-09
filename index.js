74a60ebe9868280fdc9253e0510ef3805ba555c1
YA <vector_cp@mail.ru>
2021-05-09 13:25:52 +0300

    add deco function

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import * as fs from "fs"
 import { pipeline } from "stream/promises"
 import chalk from "chalk"
-import {promisify} from "util"
+import { promisify } from "util"
 
 import handleData from "./handleData.js"
 import encodeCaesar from "./encodeCaesar.js"
+import decodeCaesar from "./decodeCaesar.js"
 
 const rawArgs = process.argv
 const args = handleData(rawArgs)
 console.log(args)
+
 
 if (!args.action || !args.shift) {
   process.exitCode = 1
@@ -21,14 +23,14 @@ if (!args.action || !args.shift) {
   process.exit()
 }
 
-const run = async (inputSource, outputSource) => {
-  console.log('run')
+
+const run = async (inputSource, outputSource, caesar) => {
   await pipeline(
     inputSource,
     async function* (source) {
       source.setEncoding("utf8")
       for await (const chunk of source) {
-        yield encodeCaesar(chunk, args.shift)
+        yield caesar(chunk, args.shift)
       }
     },
     outputSource
@@ -39,12 +41,13 @@ const run = async (inputSource, outputSource) => {
   )
 }
 
+const caesarFunc = args.action.toLowerCase() === "encode" ? encodeCaesar : decodeCaesar
+
 let inputSteam = null
 let outputStream = null
 
-const existPromise = promisify(fs.exists)
 
-// existPromise(args.outputFile ).then(console.log).then(()=>existPromise(args.inputFile)).then(console.log)
+const existPromise = promisify(fs.exists)
 
 existPromise(args.inputFile)
   .then((exist) => {
@@ -54,135 +57,10 @@ existPromise(args.inputFile)
   })
   .then(() => existPromise(args.outputFile))
   .then((exist) => {
-    console.log('args.outputFile -->', args.outputFile, exist)
     if (exist) {
-      outputStream = fs.createWriteStream(args.outputFile)
+      outputStream = fs.createWriteStream(args.outputFile, {
+        flag: "a",      
+      })
     } else outputStream = process.stdout
   })
-
-  .then(() => run(inputSteam, outputStream))
-
-
-
-
-
-
-
-// if (!args.outputFile) {outputStream = process.stdout}
-
-// if (!args.inputFile) {inputSteam = process.stdin}
-
-// const checkFiles =  async () => {
-  
-//   if (args.inputFile) {
-//   fs.stat(args.inputFile, (error) => {
-//       if (error) {
-//         process.exitCode = 1
-//         process.stderr.write(
-//           chalk.magentaBright.inverse(` Error `) +
-//           ` Can't find file ${args.inputFile}`
-//           )
-//           process.exit()
-//         } else {inputSteam = fs.createWriteStream(args.outputFile)}
-//   })
-//   }
-
-// }
-
-
-
-
-// console.log("args.outputFile", args.outputFile);
-// if (args.outputFile) {
-//   fs.stat(args.outputFile, (error) => {
-//     if (error) {
-//       process.exitCode = 1
-//       process.stderr.write(
-//         chalk.magentaBright.inverse(` Error `) +
-//         ` Can't find file ${args.outputFile}`
-//         )
-//         process.exit()
-//       } else {outputStream = fs.createWriteStream(args.outputFile)}
-//     })
-//   } else {outputStream = process.stdout}
-
-  
-//   console.log(" args.inputFile", args.inputFile);
-// if (args.inputFile) {
-//   console.log(" -----------------args.inputFile--------------", args.inputFile);
-//   fs.stat(args.inputFile, (error) => {
-//     if (error) {
-//       process.exitCode = 1
-//       process.stderr.write(
-//         chalk.magentaBright.inverse(` Error `) +
-//         ` Can't find file ${args.inputFile}`
-//         )
-//         process.exit()
-//       } else { console.log('else'); inputSteam = fs.createReadStream(args.inputFile)}
-//     })
-//   } else { console.log('else else')
-//   inputSteam = process.stdout}
-
-
-//   console.log("inp--------->",inputSteam,"out---------->", outputStream);
-
-
-
-
-  
-
-
-    
-
-
-
-//       if (args.outputFile) {
-//         fs.access(args.outputFile, function (error) {
-//           if (error) {
-//             process.exitCode = 1
-//             process.stderr.write(
-//               chalk.magentaBright.inverse(` Error `) +
-//                 ` Can't find file ${args.outputFile}`
-//             )
-//             process.exit()
-//           } else {
-//             fs.appendFile(
-//               args.outputFile,
-//               encString,
-//               (err) => {
-//                 if (err) {
-//                   process.exitCode = 1
-//                   process.stderr.write(
-//                     chalk.magentaBright.inverse(` Error `) +
-//                       ` Can't write to file ${args.outputFile}`
-//                   )
-//                   process.exit()
-//                 }
-//                 process.stdout.write(
-//                   chalk.greenBright.inverse(` Success `) +
-//                     ` ${args.outputFile} saved`
-//                 )
-//               }
-//             )
-//           }
-//         })
-//       } else
-//         process.stdout.write(
-//           chalk.greenBright.inverse(` Encoded Output `) +
-//             ` ${encString}`
-//         )
-//     })
-//   }
-// })
-
-// console.log(rawArgs);
-// process.on('exit', (code) => {
-//   console.log(`About to exit with code: ${code}`);
-// });
-
-// const __filename = new URL('', import.meta.url).pathname;
-// // Will contain trailing slash
-// const __dirname = new URL('.', import.meta.url).pathname;
-// console.log('sss', __dirname);
-
-// https://abakbot.ru/online-5/301-rus-eng-alphabet
+  .then(() => run(inputSteam, outputStream ,caesarFunc))
